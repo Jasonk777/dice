1c16a45b2cfa20dfe7400ac82e198cac35837de8
Jasonk777 <92895154+Jasonk777@users.noreply.github.com>
2022-11-23 16:52:57 -0500

    Add files via upload

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,1 +1,41 @@
 import random
+response = "y"
+while response == "y":
+    no = random.randint(1,6) 
+      
+    if no == 1: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[  0  ]") 
+        print("[-----]") 
+    if no == 2: 
+        print("[-----]") 
+        print("[   0 ]") 
+        print("[ 0   ]") 
+        print("[-----]") 
+    if no == 3: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[-----]") 
+    if no == 4: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 5: 
+        print("[-----]") 
+        print("[000  ]") 
+        print("[00   ]") 
+        print("[-----]") 
+    if no == 6: 
+        print("[-----]") 
+        print("[0 0 0]") 
+        print("[0 0 0]") 
+        print("[-----]") 
+          
+    
+
+
+    response=input("y to roll again") 
+    print("n") 
